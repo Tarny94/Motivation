eb792387c90dd0b2ef169d3d7c29fdecfa87a6bb
Alexandru.Tarnicer <alexandru.tarnicer@theaccessgroup.com>
2023-08-20 12:10:49 +0300

    added new files

--- mc/src/app/app.module.ts
+++ mc/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstPageComponent } from './pages/first-page/first-page.component';
+import { InputComponent } from './model/input/input.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { ChallengePageComponent } from './pages/challenge-page/challenge-page.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTabsModule} from '@angular/material/tabs';
+import { LoginPageComponent } from './pages/authentication/login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstPageComponent,
+    InputComponent,
+    ChallengePageComponent,
+    LoginPageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSlideToggleModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
