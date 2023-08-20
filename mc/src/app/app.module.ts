ec2b4945a200271f845b2984e3528a19426c85c3
Alexandru.Tarnicer <alexandru.tarnicer@theaccessgroup.com>
2023-08-20 13:16:28 +0300

    added uncomited changes

--- mc/src/app/app.module.ts
+++ mc/src/app/app.module.ts
@@ -11,6 +11,12 @@ import { ChallengePageComponent } from './pages/challenge-page/challenge-page.co
 import {MatButtonModule} from '@angular/material/button';
 import {MatTabsModule} from '@angular/material/tabs';
 import { LoginPageComponent } from './pages/authentication/login-page/login-page.component';
+import { ChallangePageComponent } from './model/challange-page/challange-page.component';
+import { TaskPageComponent } from './model/task-page/task-page.component';
+import { HistoryChallengesComponent } from './model/history-challenges/history-challenges.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select'
+import { MatFormFieldModule } from '@angular/material/form-field'
 
 @NgModule({
   declarations: [
@@ -19,6 +25,9 @@ import { LoginPageComponent } from './pages/authentication/login-page/login-page
     InputComponent,
     ChallengePageComponent,
     LoginPageComponent,
+    ChallangePageComponent,
+    TaskPageComponent,
+    HistoryChallengesComponent,
   ],
   imports: [
     BrowserModule,
@@ -27,7 +36,11 @@ import { LoginPageComponent } from './pages/authentication/login-page/login-page
     BrowserAnimationsModule,
     MatInputModule,
     MatButtonModule,
-    MatTabsModule
+    MatTabsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
